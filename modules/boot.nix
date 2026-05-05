d538495e6c5aaa1e01b79cd1b825611aadd904eb
Remilia Litjens <omgkawaiiangel077@gmail.com>
2026-05-05 21:02:00 +0200

    updated home manager

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,21 +1,29 @@
-{ config, pkgs, ...
-}:
+{ config, pkgs, ... }:
 
+let
+  grubTheme = pkgs.stdenv.mkDerivation {
+    name = "grub-theme";
 
+    src = /etc/nixos/.dotfiles/GrubTheme;
+
+    installPhase = ''
+      mkdir -p $out
+      cp -r . $out/
+    '';
+  };
+
+in
 {
   boot.loader.systemd-boot.enable = false;
   boot.loader.efi.canTouchEfiVariables = false;
   boot.kernelPackages = pkgs.linuxPackages_zen;
 
-
-  environment.etc."grub-theme".source =
-  /etc/nixos/.dotfiles/GrubTheme;
-
   boot.loader.grub = {
-  enable = true;
-  device = "nodev";
-  efiSupport = true;
-  efiInstallAsRemovable = true;
-};
+    enable = true;
+    device = "nodev";
+    efiSupport = true;
+    efiInstallAsRemovable = true;
 
-  }
+    theme = grubTheme;
+  };
+}
