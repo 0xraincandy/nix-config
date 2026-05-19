d64a03436695f7ef3c8820978a636a9ff8318af6
Lesley Litjens <omgkawaiiangel077@gmail.com>
2026-05-19 18:17:49 +0200

    lain theme sddm

--- modules/plasma-ly.nix
+++ modules/plasma-ly.nix
@@ -1,30 +1,40 @@
-{ config, pkgs, ...
-}:
+{ config, pkgs, ... }:
 
 {
-
   services.xserver.enable = true;
 
-  services.displayManager.ly = {
+  services.displayManager.sddm = {
     enable = true;
+    theme = "sddm-lain-wired-theme";
 
-    settings = {
-      lang = "nl";
-      numlock = true;
-    };
+
+    extraPackages = with pkgs.kdePackages; [
+      qtmultimedia
+      qt5compat
+    ];
   };
 
-  environment.etc."ly/lang/nl.ini".source =
-    ./../.dotfiles/ly/nl.ini;
+
+  environment.systemPackages = [
+    (pkgs.stdenv.mkDerivation {
+      name = "sddm-lain-wired-theme";
+      src = ./../.dotfiles/sddm-lain-wired-theme;
+
+      dontBuild = true;
+      installPhase = ''
+        mkdir -p $out/share/sddm/themes/sddm-lain-wired-theme
+        cp -r $src/* $out/share/sddm/themes/sddm-lain-wired-theme/
+      '';
+    })
+  ];
 
   services.desktopManager.plasma6.enable = true;
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
-  plasma-browser-integration
-  konsole
-  elisa
-  discover
-  okular
-  qrca
-];
-
+    plasma-browser-integration
+    konsole
+    elisa
+    discover
+    okular
+    qrca
+  ];
 }
